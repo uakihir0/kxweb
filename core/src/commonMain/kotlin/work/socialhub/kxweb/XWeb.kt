131f4cffbb72f67e170539818595a1c110f7dccc
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2026-03-11 02:35:35 +0900

    Register all new resources in XWeb interface and XWebImpl
    Add home(), user(), engagement(), post(), follow(), bookmark(),
    list(), media(), explore(), and timeline() accessors to the
    XWeb interface and wire up all implementations in XWebImpl.

--- core/src/commonMain/kotlin/work/socialhub/kxweb/XWeb.kt
+++ core/src/commonMain/kotlin/work/socialhub/kxweb/XWeb.kt
@@ -1,11 +1,31 @@
 package work.socialhub.kxweb
 
+import work.socialhub.kxweb.api.BookmarkResource
+import work.socialhub.kxweb.api.EngagementResource
+import work.socialhub.kxweb.api.ExploreResource
+import work.socialhub.kxweb.api.FollowResource
+import work.socialhub.kxweb.api.HomeResource
+import work.socialhub.kxweb.api.ListResource
+import work.socialhub.kxweb.api.MediaResource
+import work.socialhub.kxweb.api.PostResource
 import work.socialhub.kxweb.api.SearchResource
+import work.socialhub.kxweb.api.TimelineResource
 import work.socialhub.kxweb.api.TweetResource
+import work.socialhub.kxweb.api.UserResource
 import kotlin.js.JsExport
 
 @JsExport
 interface XWeb {
     fun search(): SearchResource
     fun tweet(): TweetResource
+    fun home(): HomeResource
+    fun user(): UserResource
+    fun engagement(): EngagementResource
+    fun post(): PostResource
+    fun follow(): FollowResource
+    fun bookmark(): BookmarkResource
+    fun list(): ListResource
+    fun media(): MediaResource
+    fun explore(): ExploreResource
+    fun timeline(): TimelineResource
 }
--- core/src/commonMain/kotlin/work/socialhub/kxweb/internal/XWebImpl.kt
+++ core/src/commonMain/kotlin/work/socialhub/kxweb/internal/XWebImpl.kt
@@ -2,8 +2,18 @@ package work.socialhub.kxweb.internal
 
 import work.socialhub.kxweb.XWeb
 import work.socialhub.kxweb.XWebConfig
+import work.socialhub.kxweb.api.BookmarkResource
+import work.socialhub.kxweb.api.EngagementResource
+import work.socialhub.kxweb.api.ExploreResource
+import work.socialhub.kxweb.api.FollowResource
+import work.socialhub.kxweb.api.HomeResource
+import work.socialhub.kxweb.api.ListResource
+import work.socialhub.kxweb.api.MediaResource
+import work.socialhub.kxweb.api.PostResource
 import work.socialhub.kxweb.api.SearchResource
+import work.socialhub.kxweb.api.TimelineResource
 import work.socialhub.kxweb.api.TweetResource
+import work.socialhub.kxweb.api.UserResource
 
 class XWebImpl(
     config: XWebConfig
@@ -11,7 +21,27 @@ class XWebImpl(
 
     private val search: SearchResource = SearchResourceImpl(config)
     private val tweet: TweetResource = TweetResourceImpl(config)
+    private val home: HomeResource = HomeResourceImpl(config)
+    private val user: UserResource = UserResourceImpl(config)
+    private val engagement: EngagementResource = EngagementResourceImpl(config)
+    private val post: PostResource = PostResourceImpl(config)
+    private val follow: FollowResource = FollowResourceImpl(config)
+    private val bookmark: BookmarkResource = BookmarkResourceImpl(config)
+    private val list: ListResource = ListResourceImpl(config)
+    private val media: MediaResource = MediaResourceImpl(config)
+    private val explore: ExploreResource = ExploreResourceImpl(config)
+    private val timeline: TimelineResource = TimelineResourceImpl(config)
 
     override fun search() = search
     override fun tweet() = tweet
+    override fun home() = home
+    override fun user() = user
+    override fun engagement() = engagement
+    override fun post() = post
+    override fun follow() = follow
+    override fun bookmark() = bookmark
+    override fun list() = list
+    override fun media() = media
+    override fun explore() = explore
+    override fun timeline() = timeline
 }
