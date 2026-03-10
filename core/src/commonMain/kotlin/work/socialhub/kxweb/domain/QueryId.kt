d40255df1762066f5494882688d9cb6606db56d7
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2026-03-11 02:21:38 +0900

    Add GraphQL query IDs for all X Web API endpoints
    Extend QueryId object with 22 new query IDs covering:
    - Home timeline (HomeTimeline, HomeLatestTimeline)
    - User lookup (UserByScreenName, AboutAccountQuery, UserTweets)
    - Social graph (Following, Followers)
    - Engagement mutations (FavoriteTweet, UnfavoriteTweet, CreateRetweet, DeleteRetweet)
    - Post mutations (CreateTweet, DeleteTweet)
    - Follow mutations (CreateFriendship, DestroyFriendship)
    - Bookmarks (Bookmarks, CreateBookmark, DeleteBookmark, BookmarkFolderTimeline)
    - Likes timeline
    - Lists (ListOwnerships, ListMemberships, ListLatestTweetsTimeline)
    - Explore (GenericTimelineById)

--- core/src/commonMain/kotlin/work/socialhub/kxweb/domain/QueryId.kt
+++ core/src/commonMain/kotlin/work/socialhub/kxweb/domain/QueryId.kt
@@ -7,9 +7,57 @@ package work.socialhub.kxweb.domain
  * without notice. When the API breaks, these IDs may need to be updated.
  *
  * Reference: https://github.com/steipete/bird (query-ids.json)
+ * Reference: https://github.com/taelin-app/twitter-cli (client.py)
  */
 object QueryId {
+    // Search
     const val SEARCH_TIMELINE = "6AAys3t42mosm_yTI_QENg"
+
+    // Tweet
     const val TWEET_DETAIL = "_NvJCnIjOW__EP5-RF197A"
     const val TWEET_RESULT_BY_REST_ID = "0aTrQMKgj95K791yXeNDRA"
+
+    // Home Timeline
+    const val HOME_TIMELINE = "c-CzHF1LboFilMpsx4ZCrQ"
+    const val HOME_LATEST_TIMELINE = "BKB7oi212Fi7kQtCBGE4zA"
+
+    // User
+    const val USER_BY_SCREEN_NAME = "1VOOyvKkiI3FMmkeDNxM9A"
+    const val ABOUT_ACCOUNT_QUERY = "zs_jFPFT78rBpXv9Z3U2YQ"
+    const val USER_TWEETS = "E3opETHurmVJflFsUBVuUQ"
+
+    // Social Graph
+    const val FOLLOWING = "zx6e-TLzRkeDO_a7p4b3JQ"
+    const val FOLLOWERS = "IOh4aS6UdGWGJUYTqliQ7Q"
+
+    // Engagement (Mutations)
+    const val FAVORITE_TWEET = "lI07N6Otwv1PhnEgXILM7A"
+    const val UNFAVORITE_TWEET = "ZYKSe-w7KEslx3JhSIk5LA"
+    const val CREATE_RETWEET = "ojPdsZsimiJrUGLR1sjUtA"
+    const val DELETE_RETWEET = "iQtK4dl5hBmXewYZuEOKVw"
+
+    // Post (Mutations)
+    const val CREATE_TWEET = "IID9x6WsdMnTlXnzXGq8ng"
+    const val DELETE_TWEET = "VaenaVgh5q5ih7kvyVjgtg"
+
+    // Follow (Mutations)
+    const val CREATE_FRIENDSHIP = "OPwKc1HXnBT_bWXfAlo-9g"
+    const val DESTROY_FRIENDSHIP = "ppXWuagMNXgvzx6WoXBW0Q"
+
+    // Bookmarks
+    const val BOOKMARKS = "VFdMm9iVZxlU6hD86gfW_A"
+    const val CREATE_BOOKMARK = "aoDbu3RHznuiSkQ9aNM67Q"
+    const val DELETE_BOOKMARK = "Wlmlj2-xzyS1GN3a6cj-mQ"
+    const val BOOKMARK_FOLDER_TIMELINE = "KJIQpsvxrTfRIlbaRIySHQ"
+
+    // Likes
+    const val LIKES = "lIDpu_NWL7_VhimGGt0o6A"
+
+    // Lists
+    const val LIST_OWNERSHIPS = "wQcOSjSQ8NtgxIwvYl1lMg"
+    const val LIST_MEMBERSHIPS = "BlEXXdARdSeL_0KyKHHvvg"
+    const val LIST_LATEST_TWEETS_TIMELINE = "RlZzktZY_9wJynoepm8ZsA"
+
+    // Explore
+    const val GENERIC_TIMELINE_BY_ID = "FS17m3eEMcYdJvjPP1Upvw"
 }
