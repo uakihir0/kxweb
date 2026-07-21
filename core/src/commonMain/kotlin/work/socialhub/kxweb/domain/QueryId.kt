6ac40778d0f8d5ceed88a5919aab7264066ae1c2
Akihiro Urushihara <a.urusihara@gmail.com>
2026-07-21 20:44:58 +0900

    fix(account): resolve current user through Viewer

--- core/src/commonMain/kotlin/work/socialhub/kxweb/domain/QueryId.kt
+++ core/src/commonMain/kotlin/work/socialhub/kxweb/domain/QueryId.kt
@@ -22,6 +22,7 @@ object QueryId {
     const val HOME_LATEST_TIMELINE = "BKB7oi212Fi7kQtCBGE4zA"
 
     // User
+    const val VIEWER = "u4ni7JqpqdAQxWQfkLsdUQ"
     const val USER_BY_SCREEN_NAME = "2qvSHpkWTMS9i0zJAwDNiA"
     const val ABOUT_ACCOUNT_QUERY = "zs_jFPFT78rBpXv9Z3U2YQ"
     const val USER_TWEETS = "E3opETHurmVJflFsUBVuUQ"
