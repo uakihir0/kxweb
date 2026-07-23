f76f48c8748d21237f740b4ba3f818d758c5e242
Akihiro Urushihara <a.urusihara@gmail.com>
2026-07-23 11:35:15 +0900

    refactor(sha256): replace expect/actual with pure Kotlin implementation

--- core/src/jvmMain/kotlin/work/socialhub/kxweb/util/Sha256Util.jvm.kt
+++
@@ -1,9 +0,0 @@
-package work.socialhub.kxweb.util
-
-import java.security.MessageDigest
-
-actual object Sha256Util {
-    actual fun hash(input: ByteArray): ByteArray {
-        return MessageDigest.getInstance("SHA-256").digest(input)
-    }
-}
